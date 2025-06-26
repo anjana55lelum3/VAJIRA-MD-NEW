b2f9b628df7c45cf87465bef2a55d15b6bb4d78a
anjana55lelum3 <amarasekaralelumamarasekara@gmail.com>
2025-06-27 00:59:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,19 +7,19 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94719199757' : process.env.OWNER_NUMBER,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94726884134' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
-LOGO: process.env.LOGO || '' ,    
-FOOTER: process.env.FOOTER || '',
-LIMIT_PLUGIN: process.env.LIMIT_PLUGIN || '50' ,    
-PREMIUM_USERS: process.env.PREMIUM_USERS || '94719199757',    
+LOGO: process.env.LOGO || 'https://files.catbox.moe/6x6tfc.jpg' ,    
+FOOTER: process.env.FOOTER || '🍂🖤𝗞𝗜𝗡𝗚 𝗔𝗡𝗝𝗔𝗡𝗔 𝗕𝗕𝗛 💦🥵🍂',
+LIMIT_PLUGIN: process.env.LIMIT_PLUGIN || '5000' ,    
+PREMIUM_USERS: process.env.PREMIUM_USERS || '94726884134',    
 LANG: process.env.LANG || 'en',    
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
 MAX_SIZE: 500,
 MODE: process.env.MODE === undefined ? 'nonbutton' : process.env.MODE,
-ALIVE:  process.env.ALIVE  || '> VAJIRA MD'  ,    
+ALIVE:  process.env.ALIVE  || '> ANJANA MD'  ,    
 DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO,
-JID: process.env.JID || "",
+JID: process.env.JID || "120363419368362114@g.us",
 WACHLINK: process.env.WACHLINK || `https://whatsapp.com/channel/0029Vac3pnlBlHpXLrUBym3a`
 };
